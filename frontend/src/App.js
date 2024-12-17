06b5a73e7679b76f6b82b20766167ccef09d7df0
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-17 10:18:06 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,30 +2,24 @@ import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
-import { ThemeProvider } from "./context/ThemeContext.jsx";
 import Layout from "./components/Layout/Layout.jsx";
 import Loader from "./components/Loader/Loader.jsx";
 
-const HomePage = lazy(() =>
-  import("./pages/HomePage/HomePage.jsx")
-);
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
 
 const ContactsPage = lazy(() =>
   import("./pages/ContactsPage/ContactsPage.jsx")
 );
 
-const AboutMePage = lazy(() =>
-  import("./pages/AboutMePage/AboutMePage.jsx")
-);
+const AboutMePage = lazy(() => import("./pages/AboutMePage/AboutMePage.jsx"));
 
 const NotFoundPage = lazy(() =>
   import("./pages/NotFoundPage/NotFoundPage.jsx")
 );
 
 function App() {
-
   return (
-    <ThemeProvider>
+    <>
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Layout />}>
@@ -37,7 +31,7 @@ function App() {
         </Routes>
       </Suspense>
       <Toaster position="top-right" />
-    </ThemeProvider>
+    </>
   );
 }
 
