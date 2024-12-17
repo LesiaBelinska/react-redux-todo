2514be49087bc46c43d8bed316362aa8154ed1c9
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-17 10:18:26 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
+import store, { persistor } from "./store/store.js";
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
