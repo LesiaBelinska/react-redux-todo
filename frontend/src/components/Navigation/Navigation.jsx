f0cfc29bf68a69cd522ca1a15a1982423ba0170c
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-17 10:18:10 +0200

    Update Navigation.jsx

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,16 +1,15 @@
-import { useContext } from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
-import { ThemeContext } from "../../context/ThemeContext";
 import s from "./Navigation.module.css";
 
 const Navigation = () => {
-  const { theme } = useContext(ThemeContext);
+  const darkMode = useSelector((state) => state.theme.darkMode);
 
   return (
     <nav>
       <ul
-        className={`${s.list} ${theme === "dark" ? s.dark : s.light}`}
+        className={`${s.list} ${darkMode ? s.dark : s.light}`}
       >
         <li className={s.item}>
           <NavLink
