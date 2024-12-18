7a9a99a04630393eda890db5eafa6f03f4eff0ef
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-18 22:36:45 +0200

    Update todos-api.js

--- frontend/src/services/todos-api.js
+++ frontend/src/services/todos-api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/todos",
+  baseURL: "https://jsonplaceholder.typicode.com/todos",
   headers: { "Content-Type": "application/json" },
 });
 
