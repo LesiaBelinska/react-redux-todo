7c18b44ab528cdbd1c3cb4d4895cf80941c892d2
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-18 10:53:07 +0200

    Update TodoList.jsx

--- frontend/src/components/TodoList/TodoList.jsx
+++ frontend/src/components/TodoList/TodoList.jsx
@@ -1,24 +1,30 @@
 import TodoItem from "../TodoItem/TodoItem.jsx";
 import s from "./TodoList.module.css";
 
-const TodoList = ({todos, onToggleStatus, onDeleteTodo}) => {
+const TodoList = ({todos, onToggleStatus, onDeleteTodo, isLoading}) => {
 
     return (
-        <ul className={s.list}>
-            {todos.map((todo) => {
-                return (
-                    <TodoItem
-                        key={todo.id }
-                        id={todo.id}
-                        taskText={todo.text}
-                        taskStatus={todo.status}
-                        onToggleStatus={onToggleStatus}
-                        onDeleteTodo={onDeleteTodo}
-                    />
-                )
-            })}
-        </ul>
-    )
+      <ul className={s.list}>
+        {isLoading && <p className={s.loading}>Loading todos...</p>}
+        {!isLoading && todos.length === 0 && (
+          <p className={s.empty}>Todo list is empty.</p>
+        )}
+        {!isLoading &&
+          todos.length > 0 &&
+          todos.map((todo) => {
+            return (
+              <TodoItem
+                key={todo.id}
+                id={todo.id}
+                taskText={todo.text}
+                taskStatus={todo.status}
+                onToggleStatus={onToggleStatus}
+                onDeleteTodo={onDeleteTodo}
+              />
+            );
+          })}
+      </ul>
+    );
 }
 
 export default TodoList;
