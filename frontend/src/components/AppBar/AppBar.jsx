857577f9b0bc0c094c7031c3ef47b430053e8f70
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-21 22:43:52 +0200

    Update AppBar.jsx

--- frontend/src/components/AppBar/AppBar.jsx
+++ frontend/src/components/AppBar/AppBar.jsx
@@ -1,15 +1,19 @@
 import { useSelector } from "react-redux";
 
 import Navigation from "../Navigation/Navigation.jsx";
+import AuthNavigation from "../AuthNavigation/AuthNavigation.jsx";
+import UserMenu from "../UserMenu/UserMenu.jsx";
 import ThemeSwitcher from "../ThemeSwitcher/ThemeSwitcher";
 import s from "./AppBar.module.css";
 
 const AppBar = () => {
   const darkMode = useSelector((state) => state.theme.darkMode);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
   return (
     <header className={`${s.header} ${darkMode ? s.dark : s.light}`}>
       <Navigation />
+      {isLoggedIn ? <UserMenu /> : <AuthNavigation />}
       <ThemeSwitcher />
     </header>
   );
