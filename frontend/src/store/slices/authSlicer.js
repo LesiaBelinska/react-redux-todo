96fb41825b46557cda8a63dd940255666052d38a
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-21 22:44:17 +0200

    Create authSlicer.js

---
+++ frontend/src/store/slices/authSlicer.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loginSuccess(state) {
+      state.isLoggedIn = true;
+    },
+    logoutSuccess(state) {
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+export const { loginSuccess, logoutSuccess } = authSlice.actions;
+
+export default authSlice.reducer;
