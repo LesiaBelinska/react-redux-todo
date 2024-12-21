ce8ca04069fb1d72bcac39f93ae6eabb6f22bd59
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-21 23:52:17 +0200

    Update AuthNavigation.jsx

--- frontend/src/components/AuthNavigation/AuthNavigation.jsx
+++ frontend/src/components/AuthNavigation/AuthNavigation.jsx
@@ -1,11 +1,14 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import s from "./AuthNavigation.module.css";
 
 const AuthNavigation = () => {
+  const darkMode = useSelector((state) => state.theme.darkMode);
+
   return (
     <nav className={s.nav}>
-      <ul className={s.list}>
+      <ul className={`${s.list} ${darkMode ? s.dark : s.light}`}>
         <li className={s.item}>
           <NavLink
             to="/login"
