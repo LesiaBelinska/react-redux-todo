839e5df709c07159ee87a533ec5bd2ef4cfcf99c
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-21 23:52:26 +0200

    Update UserMenu.jsx

--- frontend/src/components/UserMenu/UserMenu.jsx
+++ frontend/src/components/UserMenu/UserMenu.jsx
@@ -1,24 +1,26 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Button } from "antd";
-import {UserOutlined} from '@ant-design/icons';
+import { UserOutlined } from "@ant-design/icons";
 
-import {logoutThunk } from "../../store/thunks/authThunk.js";
+import { logoutThunk } from "../../store/thunks/authThunk.js";
 import s from "./UserMenu.module.css";
 
 const UserMenu = () => {
   const dispatch = useDispatch();
+  const darkMode = useSelector((state) => state.theme.darkMode);
 
-    return (
-      <div className={s.menu}>
-        <UserOutlined className={s.icon} />
-        <Button
-          type="button"
-          onClick={() => dispatch(logoutThunk())}
-        >
-          Logout
-        </Button>
-      </div>
-    );
+  return (
+    <div className={`${s.menu} ${darkMode ? s.dark : s.light}`}>
+      <Button
+        className={s.button}
+        type="text"
+        onClick={() => dispatch(logoutThunk())}
+      >
+        Logout
+      </Button>
+      <UserOutlined className={s.icon} />
+    </div>
+  );
 };
 
 export default UserMenu;
