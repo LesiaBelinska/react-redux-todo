caca40cbaf2a2cbb095c57d69c014095a5275e36
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-21 12:10:17 +0200

    Create SwapiPage.jsx

---
+++ frontend/src/pages/SwapiPage/SwapiPage.jsx
@@ -0,0 +1,11 @@
+import s from "./SwapiPage.module.css";
+
+const SwapiPage = () => {
+  return (
+    <div className={s.container}>
+      <h1 className={s.title}>Swapi Page</h1>
+    </div>
+  );
+};
+
+export default SwapiPage;
