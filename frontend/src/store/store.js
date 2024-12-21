95707924c978f8d0878efa3234c5de634ac85bd1
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-21 22:44:02 +0200

    Update store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -4,6 +4,7 @@ import storage from "redux-persist/lib/storage";
 
 import todoSlice from "./slices/todoSlicer.js";
 import themeSlice from "./slices/themeSlicer.js";
+import authSlice from "./slices/authSlicer.js";
 
 const themePersistConfig = {
   key: "theme",
@@ -15,13 +16,21 @@ const todoPersistConfig = {
   storage,
 };
 
+const authPersistConfig = {
+  key: "auth",
+  storage,
+};
+
+
 const persistedThemeReducer = persistReducer(themePersistConfig, themeSlice);
 const persistedTodoReducer = persistReducer(todoPersistConfig, todoSlice);
+const persistedAuthReducer = persistReducer(authPersistConfig, authSlice);
 
 const store = configureStore({
   reducer: {
     todos: persistedTodoReducer,
     theme: persistedThemeReducer,
+    auth: persistedAuthReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
