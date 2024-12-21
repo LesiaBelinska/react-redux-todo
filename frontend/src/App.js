bca3595edef6570085c2e7e4ec377efe0ff05354
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-21 22:43:49 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
+import { PrivateRoute } from "./components/PrivateRoute/PrivateRoute.jsx";
+import { PublicRoute } from "./components/PublicRoute/PublicRoute.jsx";
+
 import Layout from "./components/Layout/Layout.jsx";
 import Loader from "./components/Loader/Loader.jsx";
 
@@ -13,6 +16,8 @@ const TodoPage = lazy(() => import("./pages/TodoPage/TodoPage.jsx"));
 
 const SwapiPage = lazy(() => import("./pages/SwapiPage/SwapiPage.jsx"));
 
+const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage.jsx"));
+
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage.jsx"));
 
 function App() {
@@ -21,10 +26,11 @@ function App() {
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="contacts" exact element={<ContactsPage />} />
-            <Route path="todo" exact element={<TodoPage />} />
-            <Route path="swapi" exact element={<SwapiPage />} />
+            <Route index element={<PublicRoute><HomePage /></PublicRoute>} />
+            <Route path="/login" element={<PublicRoute restricted><LoginPage /></PublicRoute>} />
+            <Route path="contacts" exact element={<PrivateRoute><ContactsPage /></PrivateRoute>} />
+            <Route path="todo" exact element={<PrivateRoute><TodoPage /></PrivateRoute>} />
+            <Route path="swapi" exact element={<PrivateRoute><SwapiPage /></PrivateRoute>} />
             <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
