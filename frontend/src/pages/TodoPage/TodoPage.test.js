d60eeddc7ece774c9e74a346f1d730dabbe6c3c2
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-23 23:00:01 +0200

    Update TodoPage.test.js

--- frontend/src/pages/TodoPage/TodoPage.test.js
+++ frontend/src/pages/TodoPage/TodoPage.test.js
@@ -6,20 +6,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from "../../store/slices/todoSlicer.js";
 import TodoPage from "./TodoPage.jsx";
 
-describe("TodoPage tests", () => { 
-    const testStore = configureStore({
-      reducer: {
-        todos: todosReducer,
+describe("TodoPage tests", () => {
+  const testStore = configureStore({
+    reducer: {
+      todos: todosReducer,
+    },
+    preloadedState: {
+      todos: {
+        todos: [{ id: "1", text: "New Todo", status: false }],
+        isLoading: false,
       },
-      preloadedState: {
-        todos: {
-          todos: [{ id: "1", text: "New Todo", status: false }],
-          isLoading: false,
-        },
-      },
-    });
+    },
+  });
 
-    test('1. render the title', () => {
+  test("1. render the title", () => {
     render(
       <Provider store={testStore}>
         <TodoPage />
@@ -29,55 +29,68 @@ describe("TodoPage tests", () => {
     const titleElement = screen.getByText("To-Do List");
 
     expect(titleElement).toBeInTheDocument();
-    });
+  });
 
-    test("2. Should render input and button", () => {
-      render(
-        <Provider store={testStore}>
-          <TodoPage />
-        </Provider>
-      );
+  test("2. validates todo input length", async () => {
+    render(
+      <Provider store={testStore}>
+        <TodoPage />
+      </Provider>
+    );
 
-      const inputElement = screen.getByRole("textbox");
-      const buttonElement = screen.getByText("add todo");
+    const input = screen.getByRole("textbox");
+    const addButton = screen.getByText("add todo");
 
-      expect(inputElement).toBeInTheDocument();
-      expect(buttonElement).toBeInTheDocument();
-    });
-    
-    test("3. can use letters and numbers", () => {
-      render(
-        <Provider store={testStore}>
-          <TodoPage />
-        </Provider>
-      );
+    fireEvent.change(input, { target: { value: "1234" } });
+    fireEvent.click(addButton);
 
-      const input = screen.getByRole("textbox");
-      fireEvent.change(input, { target: { value: "Test123" } });
+    const errorMessage = await screen.findByText(
+      "todo must be at least 5 characters"
+    );
+    expect(errorMessage).toBeInTheDocument();
+  });
 
-      expect(input.value).toBe("Test123");
-    });
+  test("3. create new Todo", async () => {
+    render(
+      <Provider store={testStore}>
+        <TodoPage />
+      </Provider>
+    );
 
-// test("4. create new Todo", () => {
-  
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "New Todo" } });
 
-//   render(
-//     <Provider store={testStore}>
-//       <TodoPage />
-//     </Provider>
-//   );
+    const addButton = screen.getByText("add todo");
+    fireEvent.click(addButton);
 
-//   const input = screen.getByRole("textbox");
-//   fireEvent.change(input, { target: { value: "New Todo" } });
+    await screen.findByText("New Todo");
+    const newItem = screen.getByText("New Todo");
+    expect(newItem).toBeInTheDocument();
+  });
+  test("4. can use letters and numbers", () => {
+    render(
+      <Provider store={testStore}>
+        <TodoPage />
+      </Provider>
+    );
 
-//   const addButton = screen.getByText("add todo");
-//   fireEvent.click(addButton);
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "Test123" } });
 
-//   const newItem = screen.getByText("New Todo");
-//   expect(newItem).toBeInTheDocument();
-// });
-    
+    expect(input.value).toBe("Test123");
+  });
 
-})
+  test("5. Should render input and button", () => {
+    render(
+      <Provider store={testStore}>
+        <TodoPage />
+      </Provider>
+    );
 
+    const inputElement = screen.getByRole("textbox");
+    const buttonElement = screen.getByText("add todo");
 
+    expect(inputElement).toBeInTheDocument();
+    expect(buttonElement).toBeInTheDocument();
+  });
+});
